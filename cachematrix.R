75437e3a01045170f3b369a727636aeba2ce8cc9
Uktab1 <50743583+Uktab1@users.noreply.github.com>
2019-05-27 17:41:31 +0200

    Add files via upload
    2019-05-27 Files Assignment week 3

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,28 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
+#makeCacheMatrix: This function creates a special "matrix" object that can cache its inverse.
+makeCacheMatrix <- function(x = matrix()) { #default matrix
+        m <- NULL #set initially to 0
+        set <- function(y) {  #New assignment
+                x <<- y #value in parent environment
+                m <<- NULL #If new matrix, again set to NULL
+        }
+        get <- function() x #returns maxtrix
+        setmatrix <- function(solve) m <<- solve #assigns m to parent environment
+        getmatrix <- function() m #returns m
+        list(set = set, get = get, #makes a list with the 4 functions
+             setmatrix = setmatrix,
+             getmatrix = getmatrix)
 }
-
-
-## Write a short comment describing this function
-
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+#cacheSolve: This function computes the inverse of the special "matrix" returned by 
+#makeCacheMatrix above. If the inverse has already been calculated (and the matrix has not changed), 
+#then the cachesolve should retrieve the inverse from the cache.
+cacheSolve <- function(x, ...) {  #returns the inverted matrix
+        m <- x$getmatrix() #get the matrix
+        if(!is.null(m)) { #checks if matrix had been cached
+                message("getting cached data") #if cached, returns message
+                return(m) #and returns matrix which already had been inverted
+        }
+        data <- x$get() #returns matrix defined in x
+        m <- solve(data, ...) #inverts the matrix 
+        x$setmatrix(m) #sets the inverted matrix to m
+        m #prints m (the inverted matrix)
 }
